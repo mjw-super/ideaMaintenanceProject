545fa45827eb13bf7b1ab367da5f127ee4692d33
mengjunwei520 <369425346@qq.com>
2020-09-02 19:27:38 +0800

    初始化

---
+++ idea_code/src/main/java/com/idea_xmwh/idea_code/IdeaCodeApplication.java
@@ -0,0 +1,13 @@
+package com.idea_xmwh.idea_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaCodeApplication.class, args);
+    }
+
+}
---
+++ idea_code/src/main/java/com/idea_xmwh/idea_code/app/controller/DemoController.java
@@ -0,0 +1,46 @@
+package com.idea_xmwh.idea_code.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @ClassName DemoController
+ * @Author Mengjw
+ * @date 2020.09.02 17:58
+ */
+@RestController
+@RequestMapping("/app")
+public class DemoController {
+    @GetMapping("/hello")
+    public String helloWord(String name){
+        String str = name+"你好啊！！！";
+        return str;
+    }
+    @GetMapping("/hello1")
+    public String helloWord1(String name){
+        String str = name+"你好啊！！！";
+        return str;
+    }
+    @GetMapping("/hello2")
+    public String helloWord2(String name){
+        String str = name+"你好啊！！！";
+        return str;
+    }
+    @GetMapping("/hello3")
+    public String helloWord3(String name){
+        String str = name+"你好啊！！！";
+        return str;
+    }
+    @GetMapping("/hello4")
+    public String helloWord4(String name){
+        String str = name+"你好啊！！！";
+        return str;
+    }
+    @GetMapping("/hello5")
+    public String helloWord5(String name){
+        String str = name+"你好啊！！！";
+        return str;
+    }
+}
